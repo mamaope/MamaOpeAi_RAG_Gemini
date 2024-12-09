c0a42cb6ce403668f72d6025eb39ef83da7ac34b
Kitibwa Rich <38604311+RichKitibwa@users.noreply.github.com>
2024-12-09 16:47:23 +0300

    Change patient data datatype to string (#1)

--- app/services/conversational_service.py
+++ app/services/conversational_service.py
@@ -12,9 +12,8 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 PROMPT_TEMPLATE = """
 You are an experienced doctor specializing in respiratory illnesses.
-You role is to assist another doctor in diagnosing a patient by asking **specific and concise questions** based on the patient data and reference text.
-**Strictly avoid phrases like "We need more information" or any other introductory statements. Only ask the next relevant question directly.**
-If the reference text does not contain the necessary information, respond with "Sorry, I cannot answer based on the provided information."
+You are assisting another doctor in diagnosing a patient.
+You can only use the provided reference text to guide your responses.
 
 Patient Data:
 {patient_data}
@@ -25,21 +24,21 @@ Reference Text:
 Chat History:
 {chat_history}
 =========
-Guidelines:
-- The question should be concise and relevant to the patient's symptoms and medical history.
-- Example of a direct question: "Does the patient have weight loss or swollen lymph nodes?"
+Based on the patient data and reference text:
+- If more information is needed, ask one leading question to gather relevant details.
+- If you have enough information, provide a diagnosis (TB, pneumonia, or other conditions) and suggest further tests or treatments.
 
 Response:
 """
 
-def generate_response(query: str, chat_history: str, patient_data: Dict[str, str], retriever):
+def generate_response(query: str, chat_history: str, patient_data: str, retriever):
 
     # Retrieve context
     context = retrieve_context(query, retriever)
 
     # Populate prompt
     prompt = PROMPT_TEMPLATE.format(
-        patient_data="\n".join(f"{key}: {value}" for key, value in patient_data.items()),
+        patient_data=patient_data,
         context=context,
         chat_history=chat_history,
     )
@@ -48,14 +47,7 @@ def generate_response(query: str, chat_history: str, patient_data: Dict[str, str
     # print(prompt)
     # Generate model response
     model = genai.GenerativeModel("gemini-1.5-pro-latest")
-    response = model.generate_content(
-        prompt,
-        generation_config=genai.GenerationConfig(
-            temperature=0.1,
-            top_k=10,
-            top_p=0.8
-        ),    
-    ).text
+    response = model.generate_content(prompt).text
 
     # Check if the response is complete
     diagnosis_complete = any(
