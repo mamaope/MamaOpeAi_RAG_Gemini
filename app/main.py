277534e2bb0b6dc348c7e772a0533525457b0b19
RichKitibwa <richkitibwa@gmail.com>
2025-01-02 14:57:33 +0300

    persist vector store to local storage

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.models.schemas import DiagnosisInput, DiagnosisResponse
 from app.services.conversational_service import generate_response
-from app.services.vectordb_service import load_documents_to_db
+from app.services.vectordb_service import load_vectorstore_from_disk, save_vectorstore_to_disk
 from app.routers import diagnosis
 
 # Initialize FastAPI app
@@ -10,11 +10,20 @@ app = FastAPI(
     description="API for conversational diagnosis using FAISS vector store and Gemini as base model."
 )
 
-app.include_router(diagnosis.router, prefix="/api/v1", tags=["Diagnosis"])
+@app.on_event("startup")
+async def startup_event():
+    print("Starting up...")
+    # Load the vector store
+    vectorstore = load_vectorstore_from_disk()
+    if not vectorstore:
+        print("Vector store not found. Building a new one...")
+        save_vectorstore_to_disk()
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the MamaOpe AI RAG API!"}
+
+app.include_router(diagnosis.router, prefix="/api/v1", tags=["Diagnosis"])
 
 if __name__ == "__main__":
     import uvicorn
