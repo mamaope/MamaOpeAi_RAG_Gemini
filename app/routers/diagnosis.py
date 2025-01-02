92f067a7c13ebb00f52c0469d11097ad842dece2
Kitibwa Rich <38604311+RichKitibwa@users.noreply.github.com>
2025-01-02 15:00:08 +0300

    persist vector store to local storage (#2)

--- app/routers/diagnosis.py
+++ app/routers/diagnosis.py
@@ -1,23 +1,34 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from app.models.schemas import DiagnosisInput, DiagnosisResponse
 from app.services.conversational_service import generate_response
-from app.services.vectordb_service import load_documents_to_db
+from app.services.vectordb_service import load_vectorstore_from_disk
 
 # Initialize router
 router = APIRouter()
 
-# Load FAISS vector store and retriever
-vectorstore = load_documents_to_db()
-if not vectorstore:
-    raise Exception("Failed to load FAISS vector store.")
+vectorstore = None
 
-retriever = vectorstore.as_retriever(
-    search_type="similarity", 
-    search_kwargs={"k": 5}
-)
+def get_retriever():
+    global vectorstore
+    if not vectorstore:
+        vectorstore = load_vectorstore_from_disk()
+        if not vectorstore:
+            raise HTTPException(status_code=500, detail="Vector store failed to load.")
+    return vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+
+
+# # Load FAISS vector store and retriever
+# vectorstore = load_documents_to_db()
+# if not vectorstore:
+#     raise Exception("Failed to load FAISS vector store.")
+
+# retriever = vectorstore.as_retriever(
+#     search_type="similarity", 
+#     search_kwargs={"k": 5}
+# )
 
 @router.post("/diagnose", response_model=DiagnosisResponse)
-async def diagnose(data: DiagnosisInput):
+async def diagnose(data: DiagnosisInput, retriever=Depends(get_retriever)):
     try:
         query = " ".join(data.patient_data)
         response, diagnosis_complete = generate_response(
